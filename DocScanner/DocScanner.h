b49fa1e6bf9b9074d44dbce8f09113ef6981e001
Adelina Bulai <adelina.bulai@softvision.ro>
2021-12-27 12:53:07 +0200

    Initial setup

---
+++ DocScanner/DocScanner.h
@@ -0,0 +1,18 @@
+//
+//  DocScanner.h
+//  DocScanner
+//
+//  Created by adelina.bulai on 09.12.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DocScanner.
+FOUNDATION_EXPORT double DocScannerVersionNumber;
+
+//! Project version string for DocScanner.
+FOUNDATION_EXPORT const unsigned char DocScannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DocScanner/PublicHeader.h>
+
+
---
+++ DocScanner/Menu/MenuViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MenuViewController.swift
+//  DocScanner
+//
+//  Created by adelina.bulai on 09.12.2021.
+//
+
+import Foundation
+import UIKit
+
+class MenuViewController: UIViewController {
+    
+}
