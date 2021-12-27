528d93d74a1bdbed702c9d89a7a2c57f16dea88d
Adelina Bulai <adelina.bulai@softvision.ro>
2021-12-27 15:19:18 +0200

    initial test service

---
+++ DocScanner/PublicServices/DSTestService.swift
@@ -0,0 +1,15 @@
+//
+//  DSTestService.swift
+//  DocScanner
+//
+//  Created by adelina.bulai on 27.12.2021.
+//
+
+import Foundation
+
+public class DSTestService: NSObject {
+    
+    public static func message() -> String {
+        return "Test message"
+    }
+}
